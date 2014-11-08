d6d3cbf19273c3dc062de9da6f61debadef20513
seanluciotolentino <seanluciotolentino@gmail.com>
2014-11-08 10:42:46 -0500

    Added new and iso models of gravity

--- star_simulator.py
+++ star_simulator.py
@@ -26,26 +26,49 @@ def rotate(x, y, theta):
     """
     return x*np.cos(theta) - y*np.sin(theta), y*np.cos(theta) + x*np.sin(theta)
 
-def angular_velocity(R, M):
-    ly2m=1.0/1.05702341e-16
-    return np.sqrt(cons.G*M/R/ly2m)
+def newton_angular_velocity(mass, radius):
+    return np.sqrt(cons.G*mass/radius/ly2m)
+    
+def iso_angular_velocity(Rc,Vinf, radius):
+    tmp=1.0-Rc/radius/ly2m*np.arctan(radius*ly2m/Rc)
+    return Vinf*np.sqrt(tmp)
+    
+def nfw_angular_velocity(Vv,cv,Rv,R):
+    gcinv=np.log(1.0+cv)-1.0/(1.0/cv+1.0)
+    s=R/Rv/kpc2ly
+    cvs=cv*s
+    vsq=Vv*Vv/s/gcinv*(np.log(1.0+cvs)-1.0/(1.0/cvs+1))
+    return np.sqrt(vsq)
 
 #Will be called with input as JSON
 if __name__ == "__main__":
     #set parameters
     #parameters = sys.argv[0]
-    parameters = {"dark_matter":False, "amount_dark_matter": 6, "distribution": "Scenario A"}
+    parameters = {"dark_matter":False, "model": "ISO", "amount_dark_matter": 6, "distribution": "Scenario A"}
     width = 50
-    number_of_stars = 20
-    mass = 1e30
-    timesteps = 100
+    number_of_stars = 5
+    timesteps = 2
     lambda_ = 0.2  # exponential distribution of stars
+    
+    #constants
+    kpc2ly=3261.63344
+    ly2m=1.0/1.05702341e-16
+    H=2.1e-18
+    kpc2m=3e19
+    Msun=2e30 #kg
+    gal='M33'
+    Mv=2e11*Msun
+    tmp=4.0/3.0*np.pi*2e-24
+    Rv=np.power(Mv/tmp,0.33)*5
+    M={'M33':1e40, 'Vinf':105400, 'Rc':1.39*kpc2m, 'Vv':H*10*Rv, 'cv':4.0,'Rv':Rv/kpc2m}
+    
     
     #exponentially distribute stars from center of galaxy
     stars_r = [width*random.exponential(lambda_) for i in range(number_of_stars)]
     stars_theta = [random.uniform(2.0)*np.pi for i in range(number_of_stars)]
     stars_x = [stars_r[i]*np.cos(stars_theta[i]) for i in range(number_of_stars)]
     stars_y = [stars_r[i]*np.sin(stars_theta[i]) for i in range(number_of_stars)]
+    
     
     #BUILD JSON
     positions = {0:{i:(stars_x[i], stars_y[i]) for i in range(number_of_stars)}} # save initial positions
@@ -54,10 +77,12 @@ if __name__ == "__main__":
         for s in range(number_of_stars):
             #rotate star's position
             if not parameters["dark_matter"]:
-                new_angle = angular_velocity(stars_r[s], mass) * stars_r[s] * 2
-                stars_x[s], stars_y[s] = rotate(stars_x[s], stars_y[s], new_angle)
-            else:
-                pass
+                new_angle = newton_angular_velocity(M[gal], stars_r[s]) * stars_r[s] * 2
+            elif parameters["model"]=="ISO":
+                new_angle = iso_angular_velocity(M['Rc'],M['Vinf'],stars_r[s])
+            elif parameters["model"]=="NFW":
+                new_angle = nfw_angular_velocity(M['Vv'],M['cv'],M['Rv'],stars_r[s])
+            stars_x[s], stars_y[s] = rotate(stars_x[s], stars_y[s], new_angle)
             #save position
             positions[t][s] = (stars_x[s], stars_y[s])
     json.dumps(positions)
@@ -65,14 +90,17 @@ if __name__ == "__main__":
     #VISUALIZE
     #plt.close('all')
     #orbits
-    colors = ['r', 'b', 'g', 'k', 'y', 'c', 'm',]
+    colors = ['r', 'b', 'g', 'k', 'c', 'm',]
     plt.figure(figsize=(6,6))
     for s in range(number_of_stars):
         x = [positions[t][s][0] for t in range(timesteps)]
         y = [positions[t][s][1] for t in range(timesteps)]
         plt.scatter(x,y,c = colors[s%len(colors)], linewidth=0)
-        plt.xlim((-width*1.1,width*1.1))
-        plt.ylim((-width*1.1,width*1.1))
+    #add universe center
+    plt.scatter([0], [0], c = 'y', s=60)
+    #set graph properties
+    plt.xlim((-width*1.1,width*1.1))
+    plt.ylim((-width*1.1,width*1.1))
     #radius distribution
     #plt.figure()
     #plt.hist([stars_r[s] for s in range(number_of_stars)])
